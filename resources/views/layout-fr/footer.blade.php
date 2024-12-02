0463fd7c50407f2a7638b07badf228f5a6425101
Vithyasagar@arda <vithyasagar@technoarete.org>
2024-12-02 18:37:51 +0530

    changes

--- resources/views/layout-fr/footer.blade.php
+++ resources/views/layout-fr/footer.blade.php
@@ -8,13 +8,13 @@
                         <div class="col-md-12">
                             <ul class="foot-ul">
                                 <h4 class="lin">Liens rapides</h4>
-                                <li><a href="{{ route('about') }}">À propos de nous</a></li>
+                                <li><a href="{{ route('about-fr') }}">À propos de nous</a></li>
                                 <li><a href="{{ route('blog') }}">Blogue</a></li>
-                                <li><a href="{{ route('add-event') }}">Ajouter un événement</a></li>
-                                <li><a href="{{ route('video-conferences') }}">Vidéoconférence</a></li>
+                                <li><a href="{{ route('add-event-fr') }}">Ajouter un événement</a></li>
+                                <li><a href="{{ route('video-conferences-fr') }}">Vidéoconférence</a></li>
                                 <li><a href="https://www.ardaconference.com/journal.php/">Journaux</a></li>
-                                <li><a href="{{ route('contact') }}">Contactez-nous</a></li>
-                                <li><a href="{{ route('sitemap') }}">Plan du site</a></li>
+                                <li><a href="{{ route('contact-fr') }}">Contactez-nous</a></li>
+                                <li><a href="{{ route('sitemap-fr') }}">Plan du site</a></li>
                             </ul>
                         </div>
                     </div>
@@ -24,13 +24,13 @@
                         <div class="col-md-12">
                             <ul class="foot-ul">
                                 <h4 class="lin">Sujets populaires</h4>
-                                <li><a href="{{ route('topic-page', ['topic' => 'dentistry']) }}" target="_blank">Dentisterie</a></li>
-                                <li><a href="{{ route('topic-page', ['topic' => 'pharmacy']) }}" target="_blank">Pharmacie</a></li>
-                                <li><a href="{{ route('topic-page', ['topic' => 'nanotechnology']) }}" target="_blank">Nanotechnologie</a></li>
-                                <li><a href="{{ route('topic-page', ['topic' => 'nursing']) }}" target="_blank">Allaitement</a></li>
-                                <li><a href="{{ route('topic-page', ['topic' => 'health']) }}" target="_blank">Santé</a></li>
-                                <li><a href="{{ route('topic-page', ['topic' => 'oncology']) }}" target="_blank">Oncologie</a></li>
-                                <li><a href="{{ route('topic-page', ['topic' => 'earth-sciences']) }}" target="_blank">Sciences de la Terre</a>
+                                <li><a href="{{ route('topic-page-fr', ['topic' => 'dentistry']) }}" target="_blank">Dentisterie</a></li>
+                                <li><a href="{{ route('topic-page-fr', ['topic' => 'pharmacy']) }}" target="_blank">Pharmacie</a></li>
+                                <li><a href="{{ route('topic-page-fr', ['topic' => 'nanotechnology']) }}" target="_blank">Nanotechnologie</a></li>
+                                <li><a href="{{ route('topic-page-fr', ['topic' => 'nursing']) }}" target="_blank">Allaitement</a></li>
+                                <li><a href="{{ route('topic-page-fr', ['topic' => 'health']) }}" target="_blank">Santé</a></li>
+                                <li><a href="{{ route('topic-page-fr', ['topic' => 'oncology']) }}" target="_blank">Oncologie</a></li>
+                                <li><a href="{{ route('topic-page-fr', ['topic' => 'earth-sciences']) }}" target="_blank">Sciences de la Terre</a>
                                 </li>
                             </ul>
                         </div>
@@ -41,13 +41,13 @@
                         <div class="col-md-12">
                             <ul class="foot-ul">
                                 <h4 class="lin">Pays populaires</h4>
-                                <li><a href="{{ route('country-page', ['country' => 'andorra']) }}">Andorre</a></li>
-                                <li> <a href="{{ route('country-page', ['country' => 'belgium']) }}">Belgique</a></li>
-                                <li> <a href="{{ route('country-page', ['country' => 'croatia']) }}">Croatie</a></li>
-                                <li> <a href="{{ route('country-page', ['country' => 'denmark']) }}">Danemark</a></li>
-                                <li> <a href="{{ route('country-page', ['country' => 'estonia']) }}">Estonie</a></li>
-                                <li> <a href="{{ route('country-page', ['country' => 'finland']) }}">Finlande</a></li>
-                                <li> <a href="{{ route('country-page', ['country' => 'germany']) }}">Allemagne</a></li>
+                                <li><a href="{{ route('country-page-fr', ['country' => 'andorra']) }}">Andorre</a></li>
+                                <li> <a href="{{ route('country-page-fr', ['country' => 'belgium']) }}">Belgique</a></li>
+                                <li> <a href="{{ route('country-page-fr', ['country' => 'croatia']) }}">Croatie</a></li>
+                                <li> <a href="{{ route('country-page-fr', ['country' => 'denmark']) }}">Danemark</a></li>
+                                <li> <a href="{{ route('country-page-fr', ['country' => 'estonia']) }}">Estonie</a></li>
+                                <li> <a href="{{ route('country-page-fr', ['country' => 'finland']) }}">Finlande</a></li>
+                                <li> <a href="{{ route('country-page-fr', ['country' => 'germany']) }}">Allemagne</a></li>
                             </ul>
                         </div>
                     </div>
@@ -57,13 +57,13 @@
                         <div class="col-md-12">
                             <ul class="foot-ul">
                                 <h4 class="lin">Villes populaires</h4>
-                                <li> <a href="{{ route('city-page', ['city' => 'yerevan']) }}">Erevan</a></li>
-                                <li> <a href="{{ route('city-page', ['city' => 'vienna']) }}">Vienne</a></li>
-                                <li> <a href="{{ route('city-page', ['city' => 'minsk']) }}">Minsk</a></li>
-                                <li> <a href="{{ route('city-page', ['city' => 'bruges']) }}">Bruges</a></li>
-                                <li><a href="{{ route('city-page', ['city' => 'sofia']) }}">Sofia</a></li>
-                                <li><a href="{{ route('city-page', ['city' => 'zagreb']) }}}">Zagreb</a></li>
-                                <li> <a href="{{ route('city-page', ['city' => 'nicosia']) }}">Nicosie</a></li>
+                                <li> <a href="{{ route('city-page-fr', ['city' => 'yerevan']) }}">Erevan</a></li>
+                                <li> <a href="{{ route('city-page-fr', ['city' => 'vienna']) }}">Vienne</a></li>
+                                <li> <a href="{{ route('city-page-fr', ['city' => 'minsk']) }}">Minsk</a></li>
+                                <li> <a href="{{ route('city-page-fr', ['city' => 'bruges']) }}">Bruges</a></li>
+                                <li><a href="{{ route('city-page-fr', ['city' => 'sofia']) }}">Sofia</a></li>
+                                <li><a href="{{ route('city-page-fr', ['city' => 'zagreb']) }}}">Zagreb</a></li>
+                                <li> <a href="{{ route('city-page-fr', ['city' => 'nicosia']) }}">Nicosie</a></li>
                             </ul>
                         </div>
                     </div>
